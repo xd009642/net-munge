a6986139f3801f1f94f3a4f2667cca24e57a682e
xd009642 <danielmckenna93@gmail.com>
2023-03-02 22:58:30 +0000

    Implement float compression for torch!

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::{Args, Parser, Subcommand, ValueEnum};
+use eyre::{eyre, Result};
 use std::fs;
 use std::io::{self, prelude::*};
 use std::path::PathBuf;
@@ -9,7 +10,7 @@ mod tf;
 #[cfg(feature = "tch")]
 mod torch;
 
-#[derive(Parser)]
+#[derive(Debug, Parser)]
 #[command(author, version, about, long_about = None)]
 struct Cli {
     /// Sets a custom config file specifying a configuration to use
@@ -17,29 +18,72 @@ struct Cli {
     config: Option<PathBuf>,
     /// Input neural network
     #[arg(short, long, name = "INPUT")]
-    input: Option<PathBuf>,
+    input: PathBuf,
     /// Output to save to
     #[arg(short, long, name = "OUTPUT")]
     output: Option<PathBuf>,
     #[command(flatten)]
     compress: CompressionParams,
+    #[arg(short, long)]
+    engine: Engine,
 }
 
-#[derive(Default, Parser)]
+#[derive(Debug, Default, Parser)]
 pub struct CompressionParams {
     #[arg(long)]
     /// Compress floating point values to a lower precision floating point value
     floats: Option<FloatCompression>,
 }
 
+/// Compress a floating point number (TODO think about fixed point)
 #[derive(Copy, Clone, Debug, Eq, PartialEq, ValueEnum)]
 pub enum FloatCompression {
     /// Single precision float (32 bits)
     Single,
     /// Half precision floats (16 bits)
     Half,
+    /// BFloat16 (16 bit 3 more bits in exponent than a half)
+    BFloat16,
 }
 
-fn main() {
+/// Compress a floating point number (TODO think about fixed point)
+#[derive(Copy, Clone, Debug, Eq, PartialEq, ValueEnum)]
+pub enum Engine {
+    /// Tensorflow neural network
+    Tensorflow,
+    /// Torch neural network
+    Torch,
+}
+
+fn main() -> Result<()> {
     let cli = Cli::parse();
+    println!("Running: {:?}", cli);
+
+    let output = match &cli.output {
+        Some(s) => s.clone(),
+        None => PathBuf::from(format!("{}.new", cli.input.display())),
+    };
+
+    match cli.engine {
+        Engine::Torch => {
+            cfg_if::cfg_if! {
+                if #[cfg(feature="tch")] {
+                    torch::optimise(&cli.input, &output, cli.compress)?;
+                } else {
+                    return Err(eyre!("The munger is built without torch support"));
+                }
+            }
+        }
+        Engine::Tensorflow => {
+            cfg_if::cfg_if! {
+                if #[cfg(feature="tensorflow")] {
+
+                } else {
+                    return Err(eyre!("The munger is built without torch support"));
+                }
+            }
+        }
+    }
+
+    Ok(())
 }
--- src/torch.rs
+++ src/torch.rs
@@ -1,1 +1,32 @@
 //! Operations on torch networks
+//!
+//! Is this done using VarStore?
+//!
+use crate::*;
+use eyre::Result;
+use std::path::Path;
+use tch::{nn::VarStore, Device, TrainableCModule};
+
+pub fn optimise(input: &Path, output: &Path, params: CompressionParams) -> Result<()> {
+    let device = Device::cuda_if_available();
+    let mut res = VarStore::new(device);
+
+    let model = TrainableCModule::load(input, res.root())?;
+
+    println!("{:?}", res);
+
+    match params.floats {
+        Some(FloatCompression::Single) => {
+            res.float();
+        }
+        Some(FloatCompression::Half) => {
+            res.half();
+        }
+        Some(FloatCompression::BFloat16) => {
+            res.bfloat16();
+        }
+        None => {}
+    }
+    model.save(output)?;
+    Ok(())
+}
